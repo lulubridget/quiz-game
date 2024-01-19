7c85151b83c11ae511562706b1c417b7df5bcb62
Lu Wang <99560066+lulubridget@users.noreply.github.com>
2024-01-19 14:07:23 -0800

    Update quiz_brain.py

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,3 +1,5 @@
+import html
+
 class QuizBrain():
     def __init__(self, question_list):
         self.question_number = 0
@@ -13,20 +15,20 @@ class QuizBrain():
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number+=1
-        user_answer= input(f"Q.{self.question_number}: {current_question.text} (True/False)")
+        q_text = html.unescape(current_question.text)
+        user_answer= input(f"Q.{self.question_number}: {quit} (True/False)")
         self.check_answer(user_answer=user_answer,correct_answer = current_question.answer)
         
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
             print("That's right")
-            self.score+=1
-            
+            self.score+=1        
         else:
             print("That's wrong")
     
         print(f"The correct answer was: {correct_answer}")
         print(f"Your current score is: {self.score}/{self.question_number}")
-
+        print("\n")
 
 
 
